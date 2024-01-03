34171c278e553dd6e6ca21077442cc8315d447ea
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-01-02 21:45:45 -0600

    Initial commit

---
+++ Sources/SubtitleAI/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SubtitleAI
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            const string inputFile = "";
+
+            SubtitleGenerator generator = new(inputFile);
+            FileInfo result = await generator.GenerateSubtitleAsync();
+        }
+    }
+}
---
+++ Sources/SubtitleAI/SubtitleGenerator.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace SubtitleAI
+{
+    internal class SubtitleGenerator
+    {
+        private string inputFile;
+
+        public SubtitleGenerator(string inputFile)
+        {
+            this.inputFile = inputFile;
+        }
+
+        internal async Task<FileInfo> GenerateSubtitleAsync()
+        {
+            await CheckLibrariesAsync();
+        }
+
+        private Task CheckLibrariesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
